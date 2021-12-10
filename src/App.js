11a22d589d87ed81df80b60640dd46d5f3bb1088
wei30172 <claire610323@gmail.com>
2021-12-09 17:07:24 -0800

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from 'redux'
+import * as actionCreators from "./state/action-creator/index"
+
+function App() {
+  const account = useSelector((state) => state.account)
+  const dispatch = useDispatch()
+  const { depositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch);
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1 className="acount">{ account }</h1>
+        <div className="button">
+          <button onClick={() => depositMoney(1000)}>Deposit</button>
+          <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
